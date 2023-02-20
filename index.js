911cea31877aee8d13e8473a186c8f35e5dedd81
Naser Suleiman <nasersuleiman421@gmail.com>
2023-02-19 18:50:10 -0800

    questions array

--- index.js
+++ index.js
@@ -4,3 +4,33 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "dist");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlrender");
+
+const questions = [
+  {
+    type: "list",
+    name: "role",
+    message: "What is team member's role?",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What is the team member's name?",
+  },
+
+  {
+    type: "input",
+    name: "id",
+    message: "What the team member's id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the team member's email?",
+  },
+];
